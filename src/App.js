16ddcd223caa60fc9026d0ceafa2b49544663cfa
Mahmoud Daabas <mahmouddaabas@users.noreply.github.com>
2023-05-27 11:24:04 +0200

    Added a path to /information

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from "./components/Homepage"
 import SearchPage from "./components/SearchPage";
+import SongInformation from './components/SongInformation';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='search' element={<SearchPage/>} />
+        <Route path='/search' element={<SearchPage/>} />
+        <Route path='/information/:songTitle' element={<SongInformation/>} />
       </Routes>
       </BrowserRouter>
     </div>
