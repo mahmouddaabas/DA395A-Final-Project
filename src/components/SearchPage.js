b73ac7e00d19988fb740680b73221c12113b4aac
Mahmoud Daabas <mahmouddaabas@users.noreply.github.com>
2023-05-27 10:28:09 +0200

    Added API call and SongList component

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import SongList from './SongList';
 
 export default function SearchPage() {
     //State to set and save the data from the API.
@@ -10,13 +11,27 @@ export default function SearchPage() {
     const location = useLocation();
     const searchValue = location.state?.searchValue;
 
+    //Use effect to only call the API once.
+    useEffect(() => {
+        //Request data based on the searchValue
+        /*const fetchData = async () => {
+            try {
+              const response = await axios.get(`https://api.genius.com/search?q=${searchValue}&access_token=${process.env.REACT_APP_ACCESS_TOKEN}`);
+              setData(response.data.response.hits);
+              console.log(data)
+            } catch (error) {
+              console.error(error);
+            }
+          };
+      
+          fetchData();*/
+    }, []);
+
 
     return (
         <div>
             <h1>Results for: {searchValue}</h1>
-            <ul id="song-list">
-
-            </ul>
+            <SongList data={data}/>
         </div>
     );
 }
---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SongList(props) {
+    return (
+        <div>
+            <ul id="song-list">
+                <li>TEST</li>
+                <li>TEST</li>
+                <li>TEST</li>
+            </ul>
+        </div>
+    )
+}
