d28759e910a788d1d69162aee92ae79d53b97e41
Mahmoud Daabas <mahmouddaabas@users.noreply.github.com>
2023-05-27 08:31:42 +0200

    Button click and searchbarRef added

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,39 +2,41 @@ import React, { useState, useRef } from 'react'
 import './styles/Homepage.css';
 
 
-export default function Homepage(props){
-const [searchArtist, setArtist] =useState([]);
+export default function Homepage(props) {
+  const [searchArtist, setArtist] = useState([]);
+
+  const searchBarRef = useRef();
+
+  function searchMusic() {
+    console.log(searchBarRef.current.value)
+  }
 
 
-  return(
+  return (
     <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Home</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Home</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-  
-        <li class="nav-item">
-          <a class="nav-link" href="#">Saved Artists</a>
-        </li>
-      
-      </ul>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Saved Artists</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="form-container">
+        <input type="text" placeholder="Search artist.." ref={searchBarRef}/>
+        <button type="submit" onClick={searchMusic}>Search</button>
+      </div>
     </div>
-  </div>
-</nav>
+  );
 
-<div className="form-container">
-<form>
-<input type="text" placeholder="Search artist.." />
-    <button type="submit">Search</button>
-</form>
-</div>
-</div>
-
-    );
- 
 }
