804a5411dddb2fbcfea3a31b010a9a33e191892c
Mahmoud Daabas <mahmouddaabas@users.noreply.github.com>
2023-05-27 09:52:20 +0200

    Added JQuery

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -5,6 +5,13 @@ export default function SearchPage() {
   const location = useLocation();
   const searchValue = location.state?.searchValue;
 
+  console.log(process.env.REACT_APP_ACCESS_TOKEN)
+
+  function requestDataFromAPI(){
+    //Request data based on the searchValue
+    //https://api.genius.com/search?q=searchValHere&access_token=tokenHere
+  }
+
   return (
     <div>
       <h1>Search Page</h1>
