837912a7858784a0b27052da62ec327fd0e1b5b9
Mahmoud Daabas <mahmouddaabas@users.noreply.github.com>
2023-05-29 08:16:14 +0200

    Moved the navbar into a component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -20,24 +20,6 @@ export default function Homepage(props) {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Home</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-
-              <li className="nav-item">
-                <a className="nav-link" href="#">Saved Artists</a>
-              </li>
-
-            </ul>
-          </div>
-        </div>
-      </nav>
-
       <div className="form-container">
         <input type="text" placeholder="Search artist.." onChange={handleInputChange} ref={searchBarRef}/>
         <Link to="/search" state={{ searchValue }}>
